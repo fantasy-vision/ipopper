dcda4dc4851800fac0ae148e73364de473e27a9b
franksoong <songxinkun2003@sina.com>
2020-04-26 15:38:39 +0800

    add www dir to hold assets

---
+++ www/precache-manifest.124234692c399534c4245bf06117b3f8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6befe82ea4141868eb5d3b279cfe7b",
+    "url": "/ipopper/index.html"
+  },
+  {
+    "revision": "7a172c1126ec8bfd1272e3b102d20111",
+    "url": "/ipopper/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ipopper/robots.txt"
+  },
+  {
+    "revision": "63a4ac0a3824d1793663",
+    "url": "/ipopper/static/css/app.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/ipopper/static/img/logo.png"
+  },
+  {
+    "revision": "a197cd96008f0de8dc81",
+    "url": "/ipopper/static/js/about.js"
+  },
+  {
+    "revision": "63a4ac0a3824d1793663",
+    "url": "/ipopper/static/js/app.js"
+  },
+  {
+    "revision": "6c48baf17b36c5038d02",
+    "url": "/ipopper/static/js/chunk-libs.js"
+  },
+  {
+    "revision": "b2d6b8826ba4cf3f7a6f",
+    "url": "/ipopper/static/js/runtime.js"
+  }
+]);
